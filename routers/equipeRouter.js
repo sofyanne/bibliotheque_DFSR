eab19d2f3679bf65e1494ac6d373e96328db6413
Sofyanne Badir <s.badir@outlook.com>
2022-03-25 15:00:00 +0100

    Ajout de la connexion à la base de données avec gestion des erreurs

--- routers/equipeRouter.js
+++ routers/equipeRouter.js
@@ -3,7 +3,6 @@ const express = require("express");
 const equipeRouter = express.Router();
 
 equipeRouter.use("/:nom", (req, res) => {
-  console.log(req.params);
   res.render("equipes/equipe.html.twig");
 });
 
